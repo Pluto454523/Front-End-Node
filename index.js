aa30dd86660b68fa7c0671ef607fa99e2f12124b
PLUTo <nawin3399@gmail.com>
2023-03-02 17:03:15 +0700

    update port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
-const base_url = "http://10.104.11.134:8080";
+const base_url = "http://10.104.11.134:3000";
 
 // Set the template engine
 app.set('view engine', 'ejs');
