79e50df60763b5c526f4288a8e496ea4b6e81b47
PLUTo <nawin3399#gmail.com>
2023-03-08 09:36:58 +0700

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
-const base_url = "http://202.151.188.100:8080";
+const base_url = "http://node42130-pluto.proen.app.ruk-com.cloud";
 
 // Set the template engine
 app.set('view engine', 'ejs');
